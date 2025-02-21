da72a5077ad497f7085136657b9f8d7e9bb9cfc4
Alexandre Londe <alexandrelonde.x@gmail.com>
2025-02-21 17:21:08 -0300

    feat: criando o formulário de tarefas com reactive forms

--- src/app/components/task-form/task-form.component.ts
+++ src/app/components/task-form/task-form.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { TaskService } from '../../services/task.service';
 
 @Component({
   selector: 'app-task-form',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrl: './task-form.component.scss'
 })
 export class TaskFormComponent {
+  taskForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private taskService: TaskService) {
+    this.taskForm = this.fb.group({
+      title: ['', [Validators.required, Validators.minLength(3)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.taskForm.valid) {
+      this.taskService.addTask(this.taskForm.value).subscribe(() => {
+        this.taskForm.reset();
+      });
+    }
+  }
 }

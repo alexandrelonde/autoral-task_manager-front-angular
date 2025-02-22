3b7d1dba655c4c5ba61b160991d900c861c46910
Alexandre Londe <alexandrelonde.x@gmail.com>
2025-02-22 14:48:49 -0300

    feat: criando listagem de tarefas com funcionalidade de concluir e excluir

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../../services/task.service';
+
+interface Task {
+  id: number;
+  title: string;
+  isComplete: boolean;
+}
 
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.scss'
 })
-export class TaskListComponent {
+export class TaskListComponent implements OnInit{
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit() {
+    this.loadTasks();
+  }
+
+  loadTasks() {
+    this.taskService.getTasks().subscribe(data => {
+      this.tasks = data;
+    });
+  }
+
+  toggleTaskCompletion(task: Task) {
+    this.taskService.updateTask(task.id, !task.isComplete).subscribe(() => {
+      task.isComplete = !task.isComplete;
+    });
+  }
+
+  deleteTask(id: number) {
+    this.taskService.deleteTask(id).subscribe(() => {
+      this.tasks = this.tasks.filter(task => task.id !== id);
+    });
+  }
 
 }
